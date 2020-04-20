50886af61a737019284014a52214f0cd68f16c36
uiharuayako <52899485+uiharuayako@users.noreply.github.com>
2020-04-21 02:19:31 +0800

    第四题，多线程演示

--- Study/Java/week8-thread/src/homework/Question4.java
+++ Study/Java/week8-thread/src/homework/Question4.java
@@ -2,57 +2,109 @@ package homework;
 
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.*;
-import java.util.Collection;
 
 public class Question4 extends Application{
     @Override // Override the start method in the Application class
     public void start(Stage exampleStage) {
         exampleStage.setTitle("演示多线程");
-        VBox myPane = new VBox(2);
-        Label thread1 = new Label("①");
-        Label thread2 = new Label("②");
-        myPane.getChildren().addAll(thread1, thread2);
-        HBox buttons = new HBox(4);
-        Button startDash1 = new Button("开始线程1");
-        Button startDash2 = new Button("开始线程2");
-        Button pause1 = new Button("暂停线程1");
-        Button pause2 = new Button("暂停线程2");
-        Button stop = new Button("结束");
-        buttons.getChildren().addAll(startDash1, startDash2, pause1, pause2, stop);
+        Pane myPane = new Pane();
+        Text thread1 = new Text("①");
+        thread1.setY(15);
+        thread1.setFill(Color.RED);
+        Text thread2 = new Text("②");
+        thread2.setY(35);
+        thread2.setFill(Color.RED);
+        Text sign=new Text("尚未开始");
+        sign.setY(55);
+        myPane.getChildren().addAll(thread1, thread2,sign);
+        HBox buttons = new HBox(2);
+        ToggleButton startTB = new ToggleButton("开始");
+        startTB.setOnAction(event -> {
+            if (startTB.isSelected()) {
+                startTB.setText("暂停");
+                thread1.setFill(Color.GREEN);
+                thread2.setFill(Color.GREEN);
+                sign.setText("赛跑中");
+            } else {
+                startTB.setText("继续");
+                thread1.setFill(Color.RED);
+                thread2.setFill(Color.RED);
+                sign.setText(thread1.getX()>=thread2.getX()?"1领先":"2领先");
+            }
+        });
+        Button resetButton = new Button("复位");
+        resetButton.setOnAction(event -> {
+            thread1.setX(0);
+            thread2.setX(0);
+            startTB.setSelected(false);
+            startTB.setText("开始");
+            thread1.setFill(Color.RED);
+            thread2.setFill(Color.RED);
+            sign.setText("尚未开始");
+        });
+        buttons.getChildren().addAll(startTB, resetButton);
         BorderPane thisPane = new BorderPane();
         thisPane.setTop(myPane);
         thisPane.setBottom(buttons);
-        Scene myScene = new Scene(thisPane);
+        Scene myScene = new Scene(thisPane,500,100);
         exampleStage.setScene(myScene);
         exampleStage.show();
-
-        Thread threadOne=new Thread(){
-            public void run(){
-                System.out.println("s");
-                Platform.runLater(new Runnable() {
-                    @Override
-                    public void run() {
-                            thread1.setLayoutX(thread1.getLayoutX()+10);
-                    }});
+        new Thread(() -> {
+            try {
+                while (thread1.getX() < myScene.getWidth()-30) {
+                    if (startTB.isSelected()) {
+                        Platform.runLater(new Runnable() {
+                            @Override
+                            public void run() {
+                                thread1.setX(thread1.getX() + Math.random() * 10);
+                            }
+                        });
+                    }
+                    Thread.sleep(100);
+                }
+            } catch (InterruptedException e) {
+            } finally {
+                thread1.setFill(Color.BLACK);
+                thread2.setFill(Color.BLACK);
+                startTB.setSelected(false);
+                startTB.setDisable(true);
+                resetButton.setDisable(true);
+                sign.setText("1获胜！");
             }
-        };
-
-        startDash1.setOnAction(event -> {
-
-
-            threadOne.start();
-        });
+        }).start();
+        new Thread(() -> {
+            try {
+                while (thread2.getX() < myScene.getWidth()-30) {
+                    if (startTB.isSelected()) {
+                        Platform.runLater(new Runnable() {
+                            @Override
+                            public void run() {
+                                thread2.setX(thread2.getX() + Math.random() * 10);
+                            }
+                        });
+                    }
+                    Thread.sleep(100);
+                }
+            } catch (InterruptedException e) {
+            } finally {
+                thread1.setFill(Color.BLACK);
+                thread2.setFill(Color.BLACK);
+                startTB.setSelected(false);
+                startTB.setDisable(true);
+                resetButton.setDisable(true);
+                sign.setText("2获胜！");
+            }
+        }).start();
     }
-
     public static void main(String args[]) {
         launch(args);
     }
